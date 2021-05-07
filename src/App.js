e62cc19ac568243f32998804eabada5f9b404acc
Daniel2711 <detaddeo.d@gmail.com>
2021-05-07 18:01:08 +0200

    chiamata API pokemon

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [allPokemons, setAllPokemons] = useState([])
+  const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20')
+
+  const getAllPokemons = async () => {
+    const res = await fetch(loadMore)
+    const data = await res.json()
+
+    setLoadMore(data.next)
+
+    console.log(data)
+  }
+  
+  useEffect( () => {
+    getAllPokemons()
+  }, [])
+
   return (
-    <div className="App">
+    <div className="app-contaner">
       <h1>Pokedex</h1>
+      <div className="pokemon-container">
+        <div className="all-container">
+          
+        </div>
+        <button className="load-more">Load more</button>
+      </div>
     </div>
   );
 }
